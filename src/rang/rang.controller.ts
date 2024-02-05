873e4e4f44300f153ecc8444f83ce4ad4f175eee
Sanya__Petruk <Oleksandr.Petruk@infopulse.com>
2024-02-05 11:29:39 +0200

    update(rang): add secure and fix

--- src/rang/rang.controller.ts
+++ src/rang/rang.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Post} from "@nestjs/common";
+import {Body, Controller, Get, Post, UseGuards} from "@nestjs/common";
 import {RangService} from "./rang.service";
 import {CreateRangDto} from "./dto/create-rang-dto";
 import {Rang} from "./schemas/rang.schema";
+import {AuthGuard} from "../user/auth.guard";
 
 @Controller('/rang')
 
@@ -9,12 +10,17 @@ export class RangController{
     constructor( private readonly rangService: RangService) {
 
     }
+    @UseGuards(AuthGuard)
+    @Get()
      getAllFights(): Promise<Rang[]>{
         return this.rangService.getAllFights()
     }
+    @UseGuards(AuthGuard)
+    @Get()
      getFightForUserByName(@Body() {name}: { name: string }): Promise<Rang[]> {
         return this.rangService.getFightForUserByName(name)
     }
+    @UseGuards(AuthGuard)
     @Post()
      setFight(@Body() dto: CreateRangDto): Promise<Rang> {
         return this.rangService.setFight(dto)
--- src/rang/rang.module.ts
+++ src/rang/rang.module.ts
@@ -3,9 +3,15 @@ import {RangController} from "./rang.controller";
 import {RangService} from "./rang.service";
 import {MongooseModule} from "@nestjs/mongoose";
 import {Rang, RangSchema} from "./schemas/rang.schema";
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "../user/constant/constant";
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: Rang.name, schema: RangSchema}])],
+    imports: [MongooseModule.forFeature([{name: Rang.name, schema: RangSchema}]),  JwtModule.register({
+        global: true,
+        secret: jwtConstants.secret,
+        signOptions: {expiresIn: '10h'},
+    }),],
     controllers: [RangController],
     providers: [RangService]
 })
