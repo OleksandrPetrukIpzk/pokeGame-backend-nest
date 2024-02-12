212252b401e9d5d15e451834195e2e3da01e6295
Sanya__Petruk <oleksandr.petruk@infopulse.com>
2024-02-12 13:54:12 +0200

    update(rating): update rating add acordion with list of fights fix bugs

--- src/rang/dto/create-rang-dto.ts
+++ src/rang/dto/create-rang-dto.ts
@@ -1,6 +1,10 @@
-export class CreateRangDto {
-    readonly whoStart: string
-    readonly whoDefense: string
-    readonly whoWin: string
+import {IsNotEmpty} from "class-validator";
 
+export class CreateRangDto {
+    @IsNotEmpty()
+    readonly whoStart: string;
+    @IsNotEmpty()
+    readonly whoDefence: string;
+    @IsNotEmpty()
+    readonly whoWin: string;
 }
