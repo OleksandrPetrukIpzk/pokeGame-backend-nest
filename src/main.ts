35b1269fe84070c17324d84bc9e0f0b50322b6de
Sanya__Petruk <oleksandr.petruk@infopulse.com>
2024-06-09 10:40:24 +0300

    fix(fix): fix for build

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: 'https://master--musical-unicorn-c86815.netlify.app/',
-    credentials: true,
-  }
-  app.enableCors(corsOptions);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
